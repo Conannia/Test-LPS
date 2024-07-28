0f9da7692bfcad6485342e92551497c95432cd85
Conannia <conannia17@gmail.com>
2024-07-29 04:26:41 +0700

    update

--- Auth_System_Soal_no_8/obj/Debug/net7.0/Auth_System_Soal_no_8.AssemblyInfo.cs
+++ Auth_System_Soal_no_8/obj/Debug/net7.0/Auth_System_Soal_no_8.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auth_System_Soal_no_8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49363f1072de941d87f854caea85ed2479a474f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b599afdf0df6a4b85565f898221ceb3da9f20224")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auth_System_Soal_no_8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auth_System_Soal_no_8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
